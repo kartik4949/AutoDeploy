42efb89e4c3a429567eee75dbd3f23b86a8a07c3
Sharma <kshar126@LAMU02CF9CTMD6M.uhc.com>
2021-08-23 00:00:32 +0530

    init commit for ab_testing

--- autodeploy/loader/loaders.py
+++ autodeploy/loader/loaders.py
@@ -2,10 +2,20 @@ import pickle
 
 
 class PickleLoader:
-  def __init__(self, model_path):
+  def __init__(self, model_path, multi_model = False):
     self.model_path = model_path
+    self.multi_model = multi_model
 
   def load(self):
     # TODO: do handling
-    with open(self.model_path, 'rb') as reader:
-      return pickle.load(reader)
+    try:
+      if not self.multi_model:
+        self.model_path = [self.model_path]
+      models = []
+      for model in self.model_path:
+        with open(model, 'rb') as reader:
+          models.append(pickle.load(reader))
+      return models
+    except FileNotFoundError as fnfe:
+      logger.error('model file not found...')
+      raise FileNotFoundError('model file not found ...')
--- autodeploy/service/builder.py
+++ autodeploy/service/builder.py
@@ -5,8 +5,19 @@ class InfereBuilder:
   def __init__(self, config, model):
     self.config = config
     self.model = model
+    self.multi_model = False
+    if len(model) > 1:
+      self.multi_model = True
+      logger.info('running multiple models..')
+
     _infere_class = INFERE.get(self.config.model.model_type)
-    self.infere_class = _infere_class(config, model)
+    _infere_class_instances = []
+    for mod in model:
+       _infere_class_instances.append(_infere_class(config, mod)
+
+  def _get_model_id_from_split(self):
+    return np.choice(np.arange())
+
 
   def get_inference(self, input):
     return self.infere_class.infere(input)
