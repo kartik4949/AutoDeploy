673cb354421c7e2d73e507c28042f552d37146b3
kartik <kartik4949@gmail.com>
2021-09-12 01:19:23 +0530

    added build and start scripts

---
+++ build.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# docker build commands to make docker images.
+
+helpFunction()
+{
+   echo ""
+   echo "Usage: $0 -r /app/model_dependcies/reqs.txt"
+   echo -e "\t-r Path to model dependencies reqs txt."
+   exit 1 
+}
+
+while getopts "r:" opt
+do
+   case "$opt" in
+      r ) parameterR="$OPTARG" ;;
+      ? ) helpFunction ;; 
+   esac
+done
+
+if [ -z "$parameterR" ]
+then
+   echo "Some or all of the parameters are empty";
+   helpFunction
+fi
+
+# Begin script in case all parameters are correct
+echo "$parameterR"
+docker build -t autodeploy --build-arg MODEL_REQ=$parameterR -f Dockerfile .
+#docker build -t monitor --build-arg MODEL_REQ=$parameterR  -f MonitorDockerfile .
