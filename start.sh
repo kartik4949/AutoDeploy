f5013ab6beccb35c61c6ae99eab4c068d291cefc
kartik <kartik4949@gmail.com>
2021-09-12 00:25:54 +0530

    added monitor service in docker

--- start.sh
+++ start.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-# python3 /app/autodeploy/monitor.py
 while ! nc -z rabbitmq 5672; do sleep 3; done
-python3 /app/autodeploy/deploy.py
+env CONFIG=/app/configs/iris/config.yaml python3 /app/autodeploy/deploy.py
